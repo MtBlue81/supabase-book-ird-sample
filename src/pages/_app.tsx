8ab15589f7413318179ea7d95391aca85a188ad4
Shuhei Aoyama <mt.blue81@gmail.com>
2022-01-07 19:12:02 +0900

    Add login function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
+import type { AppProps } from 'next/app';
+import { Auth } from '@supabase/ui';
+import { AuthLayout } from 'src/layout/AuthLayout';
+import { client } from 'src/lib/supabase';
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <AuthLayout>
+          <Component {...pageProps} />
+        </AuthLayout>
+      </Auth.UserContextProvider>
+    </div>
+  );
+};
 
 export default MyApp
